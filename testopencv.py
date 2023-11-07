ec804ce8901adc221d9c2f913ffaa107e5d68829
akvaithi <akvaithi@gmail.com>
2023-11-07 08:03:57 -0600

    update face

--- testopencv.py
+++ testopencv.py
@@ -1,40 +1,31 @@
-import cv2 
-import numpy as np 
-  
+import cv2
 
-image = cv2.imread("C:/Users/akvai/OneDrive/Documents/Code/Python Projects/OpenCV-Test-Project/test.jpg", cv2.IMREAD_COLOR)
+# Load the cascade
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
- 
-half = cv2.resize(image, (0, 0), fx = 0.5, fy = 0.5)
+# Define a video capture object
+vid = cv2.VideoCapture(0)
 
-cv2.imwrite("C:/Users/akvai/OneDrive/Documents/Code/Python Projects/OpenCV-Test-Project/svedImage.jpg", half)
+while(True):
+    # Capture frame-by-frame
+    ret, frame = vid.read()
 
+    # Convert color image to grayscale for Viola-Jones
+    grayscale_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-# Read image. 
-img = cv2.imread('C:/Users/akvai/OneDrive/Documents/Code/Python Projects/OpenCV-Test-Project/svedImage.jpg')
+    # Perform face detection
+    faces = face_cascade.detectMultiScale(grayscale_img, 1.1, 4)
 
-# Convert to grayscale. 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  
-# Blur using 3 * 3 kernel. 
-gray_blurred = cv2.blur(gray, (3, 3)) 
-  
-# Apply Hough transform on the blurred image. 
-detected_circles = cv2.HoughCircles(gray_blurred, cv2.HOUGH_GRADIENT, 1, 20, param1 = 50, param2 = 30, minRadius = 50, maxRadius = 100) 
-  
-# Draw circles that are detected. 
-if detected_circles is not None: 
-  
-    # Convert the circle parameters a, b and r to integers. 
-    detected_circles = np.uint16(np.around(detected_circles)) 
-  
-    for pt in detected_circles[0, :]: 
-        a, b, r = pt[0], pt[1], pt[2] 
-  
-        # Draw the circumference of the circle. 
-        cv2.circle(img, (a, b), r, (0, 255, 0), 2) 
-  
-        # Draw a small circle (of radius 1) to show the center. 
-        cv2.circle(img, (a, b), 1, (0, 0, 255), 3) 
-        cv2.imshow("Detected Circle", img) 
-        cv2.waitKey(0)
+    # Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 255), 2)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('x'):
+        break
+
+# When everything is done, release the capture
+vid.release()
+cv2.destroyAllWindows()
